60bc60730141465db42de3b49b99e2e198c4ef13
bauerpm <bauerpm@gmail.com>
2018-08-13 08:32:28 -0400

    Test post todos route

--- server/server.js
+++ server/server.js
@@ -17,10 +17,12 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    console.status(400).send(e);
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+module.exports = { app };
