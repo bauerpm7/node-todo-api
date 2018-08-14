9ad5c2654a0c5b2e4cc2203f1d9db4ce78b323a4
bauerpm <bauerpm@gmail.com>
2018-08-14 17:35:03 -0400

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -122,4 +122,12 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 module.exports = { app };
